474a7f3eec19a80b7565221ba1160de86d5c281b
Songchangyeop <61750383+Songchangyeop@users.noreply.github.com>
2021-02-19 02:40:48 +0900

    show video player

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import styles from './app.module.css';
 import SerchHeader from './components/search_header/serch_header';
+import VideoDetail from './components/video_detail/video_detail';
 import VideoList from './components/video_list/video_list';
 
 const App = ({ youtube }) => {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  };
 
   const search = (query) => {
     youtube
@@ -21,7 +27,20 @@ const App = ({ youtube }) => {
   return (
     <div className={styles.app}>
       <SerchHeader onSearch={search}></SerchHeader>
-      <VideoList videos={videos}></VideoList>
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo}></VideoDetail>
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? 'list' : 'grid'}
+          ></VideoList>
+        </div>
+      </section>
     </div>
   );
 };
